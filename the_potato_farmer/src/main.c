260366a0cf62fc65e661ca44f22f65283b74258a
Tor20 <tor.blikra@gmail.com>
2026-09-21 09:50:25 +0200

    Blinkene Lys main GPIO2

--- the_potato_farmer/src/main.c
+++ the_potato_farmer/src/main.c
@@ -1,1 +1,26 @@
-void app_main() {}
+// ESP Nodemcu-32s
+// Get LED on board to blink
+
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_GPIO GPIO_NUM_2 
+
+void app_main(void)
+{
+    // Reset and configure LED GPIO
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        // LED ON (commonly active-LOW)
+        gpio_set_level(LED_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        // LED OFF
+        gpio_set_level(LED_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
